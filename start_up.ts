c3c4e49b0734abbda030b9438b2868b3b2cad81f
luiz-davi <luizd4398@gmail.com>
2023-01-04 19:30:10 -0300

    cors

--- start_up.ts
+++ start_up.ts
@@ -1,8 +1,8 @@
 import * as express from 'express';
 import * as BodyParser from 'body-parser';
+import * as cors from 'cors';
 import NewsController from './controllers/news_controller';
 import mongoose from 'mongoose';
-
 class StartUp {
   public app: express.Application;
 
@@ -16,7 +16,17 @@ class StartUp {
     this.routes();
   }
 
+  enable_cors(){
+    const options: cors.CorsOptions = {
+      methods: 'GET, OPTIONS, PUT, POST, DELETE',
+      origin: '*'
+    }
+    
+    this.app.use(cors(options));
+  }
+
   middleware(){
+    this.enable_cors();
     this.app.use(BodyParser.json());
     this.app.use(BodyParser.urlencoded({ extended: false }));
   }
